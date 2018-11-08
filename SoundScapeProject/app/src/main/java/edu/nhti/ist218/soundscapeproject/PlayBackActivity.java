d6c55b929450b95ddcebdef1c4c5fdcee27f0a15
Dovodx <36710065+Dovodx@users.noreply.github.com>
2018-11-07 20:30:11 -0500

    Work on pausing and playing
    Playing is currently broken, seems to play and then pause immediately after, debugging in progress. Pausing works great, though (too well).

--- SoundScapeProject/app/src/main/java/edu/nhti/ist218/soundscapeproject/PlayBackActivity.java
+++ SoundScapeProject/app/src/main/java/edu/nhti/ist218/soundscapeproject/PlayBackActivity.java
@@ -1,11 +1,16 @@
 package edu.nhti.ist218.soundscapeproject;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class PlayBackActivity extends AppCompatActivity {
+
+    private MainActivity mainAct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +20,18 @@ public class PlayBackActivity extends AppCompatActivity {
         configureReturnButton();
     }
 
+    @SuppressLint("RestrictedApi")
     private void configureReturnButton() {
         Button returnButton = findViewById(R.id.buttonReturn);
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(PlayBackActivity.this, MainActivity.class);
+                int requestCode = MainActivity.PAUSE_CODE;
+                Bundle bundle = new Bundle();
+                bundle.putString("key", "pause");
+                startActivityForResult(intent, requestCode, bundle);
+
                 finish();
             }
         });
