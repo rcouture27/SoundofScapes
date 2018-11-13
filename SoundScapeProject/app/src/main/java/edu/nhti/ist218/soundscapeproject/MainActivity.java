16a3d65dbf6edbe1155cbcdff90266b7d583d409
unknown <rcouture27@gmail.com>
2018-11-12 22:16:49 -0500

    gif spinner working

--- SoundScapeProject/app/src/main/java/edu/nhti/ist218/soundscapeproject/MainActivity.java
+++ SoundScapeProject/app/src/main/java/edu/nhti/ist218/soundscapeproject/MainActivity.java
@@ -19,7 +19,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     private SeekBar primaryVolume;
     private SeekBar secondaryVolume;
@@ -33,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private static MediaPlayer secondaryPlaying = null;
     private static MediaPlayer tertiaryPlaying = null;
 
+
     private final static int maxVolume = 100;
 
     @Override
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+
 
         //initialize mp3 files
         MediaPlayer rain_sound = MediaPlayer.create(this, R.raw.rain_sound);
@@ -55,32 +61,34 @@ public class MainActivity extends AppCompatActivity {
         MediaPlayer harbor_sound = MediaPlayer.create(this, R.raw.harbor_bells_sound);
         MediaPlayer leaves_sound = MediaPlayer.create(this, R.raw.rustling_leaves_sound);
 
-
-
-
         //add mp3 to audio array
         audio = new MediaPlayer[] {rain_sound, ocean_sound, fire_sound, thunder_sound,
                             seagulls_sound, crickets_sound, birds_sound, harbor_sound, leaves_sound};
+
+
 
         //initializes string array from strings xml
         String[] primarySounds = getResources().getStringArray(R.array.primary_sounds);
         String[] secondarySounds = getResources().getStringArray(R.array.secondary_sounds);
         String[] tertiarySounds = getResources().getStringArray(R.array.tertiary_sounds);
+        String[] gifSpinnerArray = getResources().getStringArray(R.array.gif_spinner);
 
         //initializes spinners and adds them to an array
         Spinner primarySpinner = findViewById(R.id.primarySpinner);
         Spinner secondarySpinner = findViewById(R.id.secondarySpinner);
         Spinner tertiarySpinner = findViewById(R.id.tertiarySpinner);
-        Spinner[] spinners = new Spinner[] {primarySpinner, secondarySpinner, tertiarySpinner};
+        Spinner gifSpinner = findViewById(R.id.gifSpinner);
+        Spinner[] spinners = new Spinner[] {primarySpinner, secondarySpinner, tertiarySpinner, gifSpinner};
 
         //populates spinners with string array
         populateSpinner(spinners[0], primarySounds, 0);
         populateSpinner(spinners[1], secondarySounds, 3);
         populateSpinner(spinners[2], tertiarySounds, 6);
-
+        populateGifSpinner(spinners[3], gifSpinnerArray);
 
         configureStartButton();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -156,6 +164,7 @@ public class MainActivity extends AppCompatActivity {
                 String selectedItemText = (String) parent.getItemAtPosition(position);
                 // If user change the default selection
                 // First item is disable and it is used for hint
+
                 switch (position) {
                     case 1:
                         setVolumeSlider(soundNum);
@@ -175,12 +184,99 @@ public class MainActivity extends AppCompatActivity {
                     default:
                         break;
                 }
+
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
+        });
+    }
+
+
+    private void populateGifSpinner(final Spinner spinner, String[] gifArray) {
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, R.layout.support_simple_spinner_dropdown_item, gifArray){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0) {
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+
+                switch (position) {
+                    case 1:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.tornado = true;
+                        break;
+                    case 2:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.tornado2 = true;
+                        break;
+                    case 3:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.storm = true;
+                        break;
+                    case 4:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.storm2 = true;
+                        break;
+                    case 5:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.street = true;
+                        break;
+                    case 6:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.rain_ocean = true;
+                        break;
+                    case 7:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.rain = true;
+                        break;
+                    case 8:
+                        Toast.makeText(parent.getContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                        PlayBackActivity.lightning = true;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+
+                }
         });
     }
 
